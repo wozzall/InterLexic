4e657603c5fe92e086d8612f9b13bf58c69b9c94
wozzall <worrall1995@gmail.com>
2022-04-05 17:50:42 +0100

    Initial Commit

---
+++ InterLexic/InterLexicApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterLexicApp.swift
+//  InterLexic
+//
+//  Created by George Worrall on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct InterLexicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
